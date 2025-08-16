96a8cc69f945e64771250a65aa764c7dd50658d3
Idan <idan.h@squaretalk.com>
2025-08-17 00:31:25 +0300

    backend api progress

--- api/bug/bug.controller.js
+++ api/bug/bug.controller.js
@@ -5,76 +5,82 @@ import { bugService } from "./bug.service.js"
 
 // List
 export async function getBugs(req, res) {
-  try {
-    const filterBy = {
-      txt: req.query.txt || "",
-      minSpeed: +req.query.minSpeed || 0,
-      pageIdx: req.query.pageIdx || undefined,
-    }
-    const bugs = await bugService.query(filterBy)
-    res.send(bugs)
-  } catch (err) {
-    res.status(400).send(`Couldn't get bugs`)
-  }
+	try {
+		const filterBy = {
+			txt: req.query.txt || "",
+			minSpeed: +req.query.minSpeed || 0,
+			pageIdx: req.query.pageIdx || undefined,
+		}
+		const bugs = await bugService.query(filterBy)
+		res.send(bugs)
+	} catch (err) {
+		res.status(400).send(`Couldn't get bugs`)
+	}
 }
 
 // Get
 export async function getBug(req, res) {
-  const { bugId } = req.params
+	const { bugId } = req.params
 
-  try {
-    const bug = await bugService.getById(bugId)
-    res.send(bug)
-  } catch (err) {
-    res.status(400).send(`Couldn't get bug`)
-  }
+	try {
+		const bug = await bugService.getById(bugId)
+		res.send(bug)
+	} catch (err) {
+		res.status(400).send(`Couldn't get bug`)
+	}
 }
 
 //Delete
 export async function removeBug(req, res) {
-  // const loggedinUser = authService.validateToken(req.cookies.loginToken)
-  // if (!loggedinUser) return res.status(401).send('Cannot remove bug')
-  // const loggedinUser = req.loggedinUser
-  const { bugId } = req.params
+	// const loggedinUser = authService.validateToken(req.cookies.loginToken)
+	// if (!loggedinUser) return res.status(401).send('Cannot remove bug')
+	// const loggedinUser = req.loggedinUser
+	const { bugId } = req.params
 
-  try {
-    await bugService.remove(bugId, loggedinUser)
-    res.send("Deleted OK")
-  } catch (err) {
-    res.status(400).send(`Couldn't remove bug : ${err}`)
-  }
+	try {
+		// await bugService.remove(bugId, loggedinUser)
+		await bugService.remove(bugId)
+		res.send("Deleted OK")
+	} catch (err) {
+		res.status(400).send(`Couldn't remove bug : ${err}`)
+	}
 }
 
 // // Save
 export async function addBug(req, res) {
-  // const loggedinUser = authService.validateToken(req.cookies.loginToken)
-  // if (!loggedinUser) return res.status(401).send('Login first')
-  const { loggedinUser } = req
-  const { vendor, speed } = req.body
-  // Better use createBug()
+	// const loggedinUser = authService.validateToken(req.cookies.loginToken)
+	// if (!loggedinUser) return res.status(401).send('Login first')
+	// const { loggedinUser } = req
+	const { description, title, severity } = req.body
 
-  const bugToSave = { vendor, speed: +speed }
+	console.log("req.body:", req.body)
 
-  try {
-    const savedBug = await bugService.save(bugToSave, loggedinUser)
-    res.send(savedBug)
-  } catch (err) {
-    res.status(400).send(`Couldn't save bug`)
-  }
+	// Better use createBug()
+
+	const bugToSave = { description, title, severity: +severity }
+	console.log("bugToSave", bugToSave)
+	try {
+		// const savedBug = await bugService.save(bugToSave, loggedinUser)
+		const savedBug = await bugService.save(bugToSave)
+		res.send(savedBug)
+	} catch (err) {
+		console.log("Error in addBug Yoni the zain:", err)
+		res.status(400).send(`Couldn't save bug from addBug: ${err}`)
+	}
 }
 
 export async function updateBug(req, res) {
-  // const loggedinUser = authService.validateToken(req.cookies.loginToken)
-  // if (!loggedinUser) return res.status(401).send('Login first')
-  const { loggedinUser } = req
+	// const loggedinUser = authService.validateToken(req.cookies.loginToken)
+	// if (!loggedinUser) return res.status(401).send('Login first')
+	const { loggedinUser } = req
 
-  const { _id, vendor, speed, owner } = req.body
-  const bugToSave = { _id, vendor, speed: +speed, owner }
+	const { _id, vendor, speed, owner } = req.body
+	const bugToSave = { _id, vendor, speed: +speed, owner }
 
-  try {
-    const savedBug = await bugService.save(bugToSave, loggedinUser)
-    res.send(savedBug)
-  } catch (err) {
-    res.status(400).send(`Couldn't save bug`)
-  }
+	try {
+		const savedBug = await bugService.save(bugToSave, loggedinUser)
+		res.send(savedBug)
+	} catch (err) {
+		res.status(400).send(`Couldn't save bug from updateBug`)
+	}
 }
